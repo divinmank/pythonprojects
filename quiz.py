26d14fb0396c535f21df59850b453f6172594cbd
Divine <divinemankrado1@hotmail.com>
2026-01-04 15:11:25 +0000

    The creation of a quiz

---
+++ quiz.py
@@ -0,0 +1,23 @@
+score = 0
+print("Welcome to the quiz! I will ask you 5 trivia questions.")
+q1 = input("Who was Robin Hood's Second in command? ").lower().strip()
+if q1 in ["little john", "john"]:
+    score += 1
+
+
+q2 = input("Homichlophobia is the fear of what? ")
+if (q2.lower() == "fog" or q2.lower() == "humidity"):
+    score += 1
+
+q3 = input("What was the world's first antibiotic? ")
+if (q3.lower() == "penicillin"):
+    score += 1
+
+q4 = int(input("How many noses does a slug have? Numbers only "))
+if (q4 == 4):
+    score += 1
+
+q5a, q5b = input("What two colors appear on the flag of Greece? Enter your answer separated by a space. ").split()
+if ((q5a == "blue" and q5b == "white") or (q5a == "white" and q5b == "blue")):
+    score += 1
+print("You got: ", score, "/5" )
