b4bdecc435a876182d7b5f13dc5023161727c96f
Divine <divinemankrado1@hotmail.com>
2026-01-06 15:12:07 +0000

    Didn't work previously, now the game is functional.

--- num_guess/num_guess.py
+++ num_guess/num_guess.py
@@ -5,11 +5,9 @@ print("(intro message)")
 lower = int(input("Choose the lower limit to guess from. "))
 upper = int(input("Choose the upper limit to guess from. "))
 num = randint(lower, upper)
+
 x = int(input("Guess the number between "+str(lower)+" and "+str(upper)+". "))
 while (num != x):
-    retry = int(input("Guess the number between "+str(lower)+" and "+str(upper)+". "))
-    if (num != x):
-        print("Try again!")
-    else:
-        print("Correct! The number was"+str(num))
-        break
+    print("Incorrect!")
+    x = int(input("Guess the number between "+str(lower)+" and "+str(upper)+". "))
+print("Correct! The number was "+str(num))
