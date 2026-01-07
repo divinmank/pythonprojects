75646771dc9c7af683a955e751002f10a681e3b2
Divine <divinemankrado1@hotmail.com>
2026-01-07 23:31:38 +0000

    Implemented score tracker and added ending message

--- rps/rps.py
+++ rps/rps.py
@@ -3,10 +3,15 @@ comp_wins = 0
 user_wins = 0
 comp = ["rock", "paper", "scissors"]
 you = " "
-while ((you != "rock") or (you != "paper") or (you != "scissors")):
+while ((you != "rock") and (you != "paper") and (you != "scissors")):
     you = input("Rock, paper or scissors?").lower()
 if ((comp == "paper" and you == "scissors") or (comp == "rock" and you == "paper") or (comp == "scissors" and you == "rock")):
     print("You win!")
+    user_wins += 1
 else:
     print("You lose")
+    comp_wins += 1
 
+# Di
+print("Your score is: "+ str(user_wins))
+print("The computer's score is:", str(comp_wins))
